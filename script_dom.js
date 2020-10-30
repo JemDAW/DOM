188ea0665e4d0b9f0ae65f4fe0bc83d7057b558e
JemDAW <jemimamolina.alu@iespacomolla.es>
2020-10-30 19:26:37 +0100

    Funcionan todos

--- script_dom.js
+++ script_dom.js
@@ -20,7 +20,7 @@ function numeroAleatorio(){
 var botonPrimos= document.getElementById("numerosPrimos");
 
 window.addEventListener("load", crearTabla, false);
-window.addEventListener("click", colorearTabla, false);
+botonPrimos.addEventListener("click", colorearTabla, false);
 
 function crearTabla(){
     
@@ -55,12 +55,89 @@ function crearTabla(){
   tabla.setAttribute("border", "2");
     
 }
+function esPrimo(numero){
+  for(let i = 2; i< numero; i++){
+      if (numero % i === 0){
+          return false;
+      }
+  } 
+  return numero !== 1;
+}
 
 function colorearTabla(){
+  var arrayTd= document.getElementsByTagName("td");
 
+  for(let i=0; i<arrayTd.length; i++){
+    if(esPrimo(arrayTd[i].innerText)){
+      arrayTd[i].style.backgroundColor= "yellow";
+    }
+  }
 }
 
 //3
+var botonMarcar= document.getElementById("marcar");
+var botonDesmarcar= document.getElementById("desMarcar");
+var sec3= document.getElementById("s3");
+var form= document.createElement("form");
+
+for(let i=0;i<100;i++){
+  var p = document.createElement("p");
+  p.innerText= ""+numeroAleatorio();
+  var box = document.createElement("input");
+  box.setAttribute("type", "checkbox");
+  p.appendChild(box);
+  form.appendChild(p);
+}
+
+sec3.appendChild(form);
+
+botonMarcar.addEventListener("click", marcar, false);
+botonDesmarcar.addEventListener("click", desmarcar, false);
+
+function marcar(){
+  var arrayBox= document.getElementsByTagName("input");
+  for(let i=0;i<arrayBox.length;i++){
+    arrayBox[i].checked =true;
+  }
+}
+
+function desmarcar(){
+  var arrayBoxDes= document.getElementsByTagName("input");
+  for(let i=0;i<arrayBoxDes.length;i++){
+    arrayBoxDes[i].checked = false;
+  }
+}
 
 
 //4
+
+var p1= document.getElementById("p1");
+var p2= document.getElementById("p2");
+var p3= document.getElementById("p3");
+var botonReaparecer = document.getElementById("reaparecer");
+
+p1.addEventListener("click", ocultar);
+p2.addEventListener("click", ocultar);
+p3.addEventListener("click", ocultar);
+
+p1.addEventListener("dblclick", borrar);
+p2.addEventListener("dblclick", borrar);
+p3.addEventListener("dblclick", borrar);
+
+botonReaparecer.addEventListener("click", reaparecer);
+
+function ocultar(ev){
+  var esperar = setTimeout(function(){
+    ev.target.style.display = 'none';
+  }, 200);
+}
+
+function borrar(ev){
+  ev.target.parentNode.removeChild(ev.target);
+}
+
+function reaparecer(){
+  p1.style.display = 'block';
+  p2.style.display = 'block';
+  p3.style.display = 'block';
+}
